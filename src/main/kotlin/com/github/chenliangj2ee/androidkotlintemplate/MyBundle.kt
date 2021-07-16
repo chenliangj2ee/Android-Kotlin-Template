210b578576d1543331470a2804e15d5f1bb548de
GitHub Action <action@github.com>
2021-07-16 13:22:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/chenliangj2ee/androidkotlintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/chenliangj2ee/androidkotlintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.chenliangj2ee.androidkotlintemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/chenliangj2ee/androidkotlintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chenliangj2ee.androidkotlintemplate.services
+
+import com.github.chenliangj2ee.androidkotlintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
